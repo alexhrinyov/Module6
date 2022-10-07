28e22b8f3e5e1ab5114268433517ddb7e7ad1f72
alexhrinyov <hrinyou29@gmail.com>
2022-10-07 17:54:25 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pen pen = new Pen { color = "Белый", cost = 250 };
+        }
+    }
+
+    class Pen
+    {
+        public string color;
+        public int cost;
+        public Pen()
+        {
+            color = "Чёрный";
+            cost = 100;
+        }
+        public Pen(string penColor, int penCost)
+        {
+            color = penColor;
+            cost = penCost;
+        }
+    }
+}
