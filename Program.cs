cd1d062d8265402a273a4ded3a7383386f68645c
alexhrinyov <hrinyou29@gmail.com>
2022-10-09 20:51:35 +0300

    added testproj

--- Program.cs
+++ Program.cs
@@ -10,10 +10,19 @@ namespace Unit2
     {
         static void Main(string[] args)
         {
-            Pen pen = new Pen { color = "Белый", cost = 250 };
+            Pen pen1 = new Pen ("Белый",250);
+            Pen pen2 = new Pen();
+            Console.WriteLine(pen1.color + ", " + pen1.cost);
+            Console.WriteLine(pen2.color + ", " + pen2.cost);
+            Rectangle rect1 = new Rectangle(5,77);
+            Console.WriteLine(rect1.Square());
+            Rectangle rect2 = new Rectangle(5);
+            Console.WriteLine(rect2.Square( ));
+            Rectangle rect3 = new Rectangle();
+            Console.WriteLine(rect3.Square());
         }
     }
-
+    
     class Pen
     {
         public string color;
@@ -27,6 +36,30 @@ namespace Unit2
         {
             color = penColor;
             cost = penCost;
+        }
+    }
+
+    class Rectangle
+    {
+        public int a;
+        public int b;
+        
+        public int Square() => a * b;
+        public Rectangle(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public Rectangle(int side)
+        {
+            a = side;
+            b = side;
+        }
+
+        public Rectangle()
+        {
+            a = 6;
+            b = 4;
         }
     }
 }
---
+++ TestProject/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //структура
+            Console.WriteLine("Структура: ");
+            MyStruct struct1 = new MyStruct { Name = "Саша", Length = 178, Array = new int[] { 6, 1, 7, 0 } };
+            MyStruct struct2 = struct1;
+            struct1.Name = "Юля";
+            struct1.Length = 170;
+            struct1.Array[1] = 500;
+            Console.WriteLine(struct2.Name + " " + struct2.Length + " " + struct2.Array[1]);
+            //класс
+            Console.WriteLine("Класс: ");
+            MyClass class1 = new MyClass { Name = "Саша", Length = 178, Array = new int[] { 6, 1, 7, 0 } };
+            MyClass class2 = class1;
+            class1.Name = "Юля";
+            class1.Length = 170;
+            class1.Array[1] = 500;
+            Console.WriteLine(class2.Name + " " + class2.Length + " " + class2.Array[1]);
+        }
+
+        struct MyStruct
+        {
+            public string Name;
+            public int Length;
+            public int[] Array;
+        }
+
+        class MyClass
+        {
+            public string Name;
+            public int Length;
+            public int[] Array;
+        }
+
+
+    }
+}
